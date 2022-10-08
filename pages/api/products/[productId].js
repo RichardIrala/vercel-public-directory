e8139a494002c979204777d724ba6dad2cf90c3f
RichardIrala <richardirala47@hotmail.com>
2022-10-08 14:06:00 -0300

    TypeScript agregado al proyecto

--- pages/api/products/[productId].js
+++ pages/api/products/[productId].js
@@ -1,4 +1,5 @@
 export default function getProduct(req, res) {
+  console.log(req);
   res.status(200).json({
     resa: "hola",
     // En esta forma de hacer las cosas el req.query también toma los req params (que se llamarían asi en express)
