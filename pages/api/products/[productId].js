e24728f2fd6db4ba3849ed0e0904333bc003b22d
RichardIrala <richardirala47@hotmail.com>
2022-10-08 13:12:27 -0300

    segundo commit con next instalado

---
+++ pages/api/products/[productId].js
@@ -0,0 +1,7 @@
+export default function getProduct(req, res) {
+  res.status(200).json({
+    resa: "hola",
+    // En esta forma de hacer las cosas el req.query también toma los req params (que se llamarían asi en express)
+    message: req.query,
+  });
+}
